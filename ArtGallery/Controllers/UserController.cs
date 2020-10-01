c3389290b8b212e61caa1a56c8ce30ca720bd483
mjn110 <mjn220@gmail.com>
2020-10-01 15:02:54 +0330

    First commit
    در این کامیت فایل های پروژه آپلود شده است

---
+++ ArtGallery/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtGallery.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //Login action
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            //Register action
+            return View();
+        }
+
+    }
+}

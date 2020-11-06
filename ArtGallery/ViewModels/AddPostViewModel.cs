3e09fb4174bcf936613c4885f1528a6e67c2420a
amirsahifi1374 <amir.h.sahifi@gmail.com>
2020-11-06 20:46:05 +0330

    add post viwe
    قسمت ویو پست اضافه شد

---
+++ ArtGallery/ViewModels/AddPostViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtGallery.ViewModels
+{
+    public class AddPostViewModel
+    {
+        public Byte[] PostImage { get; set; }
+        
+        public string Caption { get; set; }
+
+        
+
+
+    }
+}
